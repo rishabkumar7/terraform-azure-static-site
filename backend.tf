44a2ab7f484edf643b85ac780a7df5a98ee85f26
Rishab Kumar <45825464+rishabkumar7@users.noreply.github.com>
2023-06-21 12:06:09 -0400

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,6 +3,6 @@ terraform {
     resource_group_name  = "rishabtfstate"
     storage_account_name = "rishabtfstate20440"
     container_name       = "rishabtfstate"
-    key                  = ""
+    key                  = "terraform.tfstate"
   }
 }
